f58c5f132323815c7f99c4b955f0c8b13d609a37
Damien Di Fede <dcomposer@gmail.com>
2017-12-12 23:51:49 -0600

    added some presets from the original bytebeat discoveries, rearranged the presets

--- Presets.cpp
+++ Presets.cpp
@@ -19,12 +19,216 @@ namespace Presets
 
 	const Data kPresets[] =
 	{
+		{
+			"the sierpinsky harmony",
+			50, 8, TTProjectTime,
+			0, 0, 0, 0, 0, 0, 0, 0,
+			"// t is automatically incremented before generating a sample frame." CR
+			"// this program was originally written to run at 8000kHz" CR
+			"// so we reduce the rate of t to be closer to that rate" CR
+			"t = t/5;" CR
+			"// assigning to [*] sends the value to all output channels of the program." CR
+			"[*] = t&t>>8;",
+			// watches
+			"", "", "", "", "", "", "", "", "", "",
+		},
+		
+		{
+			"the forty-two melody",
+			50, 8, TTProjectTime,
+			0, 0, 0, 0, 0, 0, 0, 0,
+			"// t is automatically incremented before generating a sample frame." CR
+			"// this program was originally written to run at 8000kHz" CR
+			"// so we reduce the rate of t to be closer to that rate" CR
+			"t = t/5;" CR
+			"// assigning to [*] sends the value to all output channels of the program." CR
+			"[*] = t*(42&t>>10);",
+			// watches
+			"", "", "", "", "", "", "", "", "", "",
+		},
+		
+		{
+			"visy's tune",
+			50, 8, TTProjectTime,
+			0, 0, 0, 0, 0, 0, 0, 0,
+			"// this program is one shown in viznut's first bytebeat video" CR
+			"// https://www.youtube.com/watch?v=GtQdIYUtAHg" CR
+			"// it was originally written to run at 8000kHz" CR
+			"// so we reduce the rate of t to be closer to that rate" CR
+			"t = t/5;" CR
+			"// assigning to [*] sends the value to all output channels of the program." CR
+			"[*] = t*((t>>9|t>>13)&25&t>>6);",
+			// watches
+			"", "", "", "", "", "", "", "", "", "",
+		},
+		
+		{
+			"little ditty",
+			50, 15, TTProjectTime,
+			0, 0, 0, 0, 0, 0, 0, 0,
+			"// here we make a rhythmic ditty by combining three signals:" CR
+			"// a frequency modulated saw wave (try changing the frequency of modulation)" CR
+			"a = (t * 128 + $(t));" CR
+			"// >> is a bit-shift operator that shifts all bits to the right" CR
+			"// by the number of bits indicated by the right hand side." CR
+			"// try changing the hard-coded 8 in here." CR
+			"b = t >> (t % (8 * w)) / w;" CR
+			"// the right hand side of >> is wrapped to 64 before the shift," CR
+			"// so this line is the same as 'c = t;'" CR
+			"c = t >> 128;" CR
+			"// finally, we use bitwise OR to combine the three values." CR
+			"// this is similar to summing floating point signals." CR
+			"[*] = a | b | c;",
+			// watches
+			"a", "b", "c", "", "", "", "", "", "", "",
+		},
+		
+		{
+			"overtone waterfall",
+			50, 17, TTProjectTime,
+			0, 0, 0, 0, 0, 0, 0, 0,
+			"// here is another example of combining multiple signals with bitwise OR." CR
+			"// first a vanilla low frequency saw" CR
+			"a = t*128;" CR
+			"// then a saw that descends through the overtone series," CR
+			"// changing pitch every 50 milliseconds" CR
+			"b = a*(32-(m/50)%32);" CR
+			"// and then a saw that ascends through the overtone series," CR
+			"// changing pitch every 100 milliseconds" CR
+			"c = a*((m/100)%64);" CR
+			"[*] = a | b | c;",
+			// watches
+			"a", "b", "c", "", "", "", "", "", "", "",
+		},
+		
+		{
+			"aggressive texture",
+			50, 15, TTProjectTime,
+			0, 0, 0, 0, 0, 0, 0, 0,
+			"// a low frequency sine wave" CR
+			"s = $(m/2000);" CR
+			"// ^ is bitwise XOR" CR
+			"a = $(t^s);" CR
+			"// you can cleary hear the held low frequency t*32 here" CR
+			"// whereas the left side of the OR is sweeping wildly" CR
+			"[*] = (t*64 + a*s) | t*32;",
+			// watches
+			"s", "a", "", "", "", "", "", "", "", "",
+		},
+		
+		// this maybe takes too long to get interesting and is just unpleasant noise mostly, might want to ditch it.
+		{
+			"blurp",
+			50, 15, TTAlways,
+			0, 0, 0, 0, 0, 0, 0, 0,
+			"// if you can stand the noise, let this one run for a long time." CR
+			"// << shifts the bits of the left hand side" CR
+			"// by the number on the right hand side modulo 64" CR
+			"a = t<<t/(1024*8);" CR
+			"b = t>>t/16;" CR
+			"c = t>>t/32;" CR
+			"// & is bitwise AND" CR
+			"e = (a | b & c);" CR
+			"// we add 1 in two places here to prevent divide-by-zero errors" CR
+			"d = t%(t/512+1) + 1;" CR
+			"// remember that a is divided by d and then multiplied by 32" CR
+			"[*] = e/d * 32;",
+			// watches
+			"a", "b", "c", "d", "e", "", "", "", "", "",
+		},
+		
+		{
+			"garbage trash",
+			50, 15, TTAlways,
+			16, 0, 0, 0, 0, 0, 0, 0,
+			"// let this one run for a while, it subtly changes over time." CR
+			"// r will increase by one every V0+1 milliseconds." CR
+			"// we add one to V0 to prevent divide-by-zero." CR
+			"r = m/(V0+1);" CR
+			"// s will cycle through [0,15] at the same rate." CR
+			"s = r%16;" CR
+			"// note here that due to operator precedence," CR
+			"// t is multiplied by s and then the modulo is applied" CR
+			"[*] = (256*s + t*s%(512*s+1)) * r;",
+			// watches
+			"r", "s", "", "", "", "", "", "", "", "",
+		},
+		
+		{
+			"nonsense can",
+			50, 15, TTAlways,
+			0, 0, 0, 0, 0, 0, 0, 0,
+			"// this demonstrates using the previous value of the program" CR
+			"// in the calculation for output value" CR
+			"a = 1 + $(m)%32;" CR
+			"b = t*128 & t*64 & t*32;" CR
+			"c = (p/16) << p%4;" CR
+			"d = $(p/128) >> p%4;" CR
+			"p = a ^ b | c | d;" CR
+			"[*] = p;",
+			// watches
+			"a", "b", "c", "d", "p", "", "", "", "", "",
+		},
+		
+		{
+			"oink oink ribbit",
+			30, 18, TTProjectTime,
+			17, 2, 45, 5, 3, 0, 0, 0,
+			"// another example of using the previous value as part of the program," CR
+			"// with several V knobs incorporated" CR
+			"o = t*128;" CR
+			"a = t*V0 >> V1;" CR
+			"b = t - V2*100;" CR
+			"c = b*64 | b*V3>>V4;" CR
+			"p = (o | a) | c | p<<12;" CR
+			"[*] = p;",
+			// watches
+			"a", "b", "c", "p", "V0", "V1", "V2", "V3", "V4", "",
+		},
+		
+		{
+			"moving average",
+			50, 17, TTAlways,
+			0, 0, 0, 0, 0, 0, 0, 0,
+			"// this sonifies the cumulative moving average for the function" CR
+			"// f(x) = x*256 ^ x*64 & x*32" CR
+			"// see https://en.wikipedia.org/wiki/Moving_average" CR
+			"x = t+1;" CR
+			"f = x*256 ^ x*64 & x*32;" CR
+			"p = p + (f - p)/x;" CR
+			"[*] = p;",
+			// watches
+			"x", "f", "p", "", "", "", "", "", "", "",
+		},
+		
+		{
+			"stereo ellipse",
+			50, 22, TTAlways,
+			20, 100, 0, 0, 0, 0, 0, 0,
+			"// this sonifies the parametric equations for an ellipse." CR
+			"// the x-coordinate is sent to the left output channel." CR
+			"// the y-coordinate is sent to the right output channel." CR
+			"// V0 and V1 can be used to control the radii of the ellipse" CR
+			"a = V0+1;" CR
+			"b = V1+1;" CR
+			"// the cosine of t*128" CR
+			"c = $((t+w/2)*128);" CR
+			"// the sine of t*128;" CR
+			"s = $(t*128);" CR
+			"// assigning to [0] sets only the first output channel (left)" CR
+			"[0] = a*c;" CR
+			"// assigning to [1] sets only the second output channel (right)" CR
+			"[1] = b*s;",
+			// watches
+			"a", "b", "c", "s", "", "", "", "", "", "",
+		},
+		
 		// name, volume, bit depth, t-mode, program text
 		{
 			"saw wave",
 			50, 16, TTWithNoteResetting,
 			0, 0, 0, 0, 0, 0, 0, 0,
-			"// t is automatically incremented before generating a sample frame." CR
+			"// t is incremented only when there is at least one active Midi Note." CR
 			"// n is set to the most recent MIDI Note On value." CR
 			"// F is a unary operator that converts its operand to a 'frequency'." CR
 			"// If F's operand is a MIDI note number," CR
@@ -33,7 +237,7 @@ namespace Presets
 			"// increasing BIT will lower the octave, decreasing BIT will raise the octave." CR
 			"// since t increases linearly, this oscillator creates a saw wave." CR
 			"// assigning to [*] sends the value to all output channels of the program." CR
-			"[*] = t*Fn",
+			"[*] = t*Fn;",
 			// watches
 			"", "", "", "", "", "", "", "", "", "",
 		},
@@ -48,7 +252,7 @@ namespace Presets
 			"// or w-1 if greater than or equal to w/2." CR
 			"// ie: #a == a%w < w/2 ? 0 : w-1" CR
 			"// this lets us turn a saw wave into a square wave" CR
-			"[*] = #(t*Fn)",
+			"[*] = #(t*Fn);",
 			// watches
 			"", "", "", "", "", "", "", "", "", "",
 		},
@@ -60,7 +264,7 @@ namespace Presets
 			"// $ is a unary operator that returns the 'sine' of its operand." CR
 			"// the 'sine' is calculated in relation to the current value of w." CR
 			"// this lets us turn a saw wave into a sine wave" CR
-			"[*] = $(t*Fn)",
+			"[*] = $(t*Fn);",
 			// watches
 			"", "", "", "", "", "", "", "", "", "",
 		},
@@ -72,7 +276,7 @@ namespace Presets
 			"// T is a unary operator that returns the 'triangle' of its operand." CR
 			"// like $ and #, the operation uses the current value of w." CR
 			"// this lets us easily turn a saw wave into a triangle wave" CR
-			"[*] = T(t*Fn)",
+			"[*] = T(t*Fn);",
 			// watches
 			"", "", "", "", "", "", "", "", "", "",
 		},
@@ -91,7 +295,7 @@ namespace Presets
 			"// so we use the less-than operator to create a 0 or 1 value" CR
 			"b = a < 8800;" CR
 			"// we then multiply w-1 by our 0 or 1 value to create the pulse wave" CR
-			"[*] = (w-1)*b",
+			"[*] = (w-1)*b;",
 			// watches
 			"a", "b", "", "", "", "", "", "", "", "",
 		},
@@ -109,7 +313,7 @@ namespace Presets
 			"// if s is odd, we want to increase the note by an octave." CR
 			"a = n + (s%2 ? 12 : 0);" CR
 			"// finally, we can create our oscillator" CR
-			"[*] = t*Fa",
+			"[*] = t*Fa;",
 			// watches
 			"s", "a", "", "", "", "", "", "", "", "",
 		},
@@ -120,7 +324,7 @@ namespace Presets
 			2, 0, 0, 0, 0, 0, 0, 0,
 			"// a saw wave can be frequency modulated by adding a sine wave to it." CR
 			"// we use the value of the V0 knob to control the modulation frequency." CR
-			"[*] = t*Fn + $(t*V0)",
+			"[*] = t*Fn + $(t*V0);",
 			// watches
 			"V0", "", "", "", "", "", "", "", "", "",
 		},
@@ -166,45 +370,6 @@ namespace Presets
 		},
 
 		{
-			"little ditty",
-			50, 15, TTProjectTime,
-			0, 0, 0, 0, 0, 0, 0, 0,
-			"// here we make a rhythmic ditty by combining three signals:" CR
-			"// a frequency modulated saw wave (try changing the frequency of modulation)" CR
-			"a = (t * 128 + $(t));" CR
-			"// >> is a bit-shift operator that shifts all bits to the right" CR
-			"// by the number of bits indicated by the right hand side." CR
-			"// try changing the hard-coded 8 in here." CR
-			"b = t >> (t % (8 * w)) / w;" CR
-			"// the right hand side of >> is wrapped to 64 before the shift," CR
-			"// so this line is the same as 'c = t;'" CR
-			"c = t >> 128;" CR
-			"// finally, we use bitwise OR to combine the three values." CR
-			"// this is similar to summing floating point signals." CR
-			"[*] = a | b | c",
-			// watches
-			"a", "b", "c", "", "", "", "", "", "", "",
-		},
-
-		{
-			"overtone waterfall",
-			50, 17, TTAlways,
-			0, 0, 0, 0, 0, 0, 0, 0,
-			"// here is another example of combining multiple signals with bitwise OR." CR
-			"// first a vanilla low frequency saw" CR
-			"a = t*128;" CR
-			"// then a saw that descends through the overtone series," CR
-			"// changing pitch every 50 milliseconds" CR
-			"b = a*(32-(m/50)%32);" CR
-			"// and then a saw that ascends through the overtone series," CR
-			"// changing pitch every 100 milliseconds" CR
-			"c = a*((m/100)%64);" CR
-			"[*] = a | b | c",
-			// watches
-			"a", "b", "c", "", "", "", "", "", "", "",
-		},
-
-		{
 			"computer music" ,
 			50, 16, TTWithNoteResetting,
 			0, 0, 0, 0, 0, 0, 0, 0,
@@ -217,115 +382,9 @@ namespace Presets
 			"// we choose a new value when p equals zero and is less than m%r" CR
 			"a = p<1 & p<m%r ? R22 : a;" CR
 			"p = m%r;" CR
-			"[*] = $(t*F(n + a))",
+			"[*] = $(t*F(n + a));",
 			// watches
 			"s", "a", "", "", "", "", "", "", "", "",
-		},
-
-		{
-			"aggressive texture",
-			50, 15, TTProjectTime,
-			0, 0, 0, 0, 0, 0, 0, 0,
-			"// a low frequency sine wave" CR
-			"s = $(m/2000);" CR
-			"// ^ is bitwise XOR" CR
-			"a = $(t^s);" CR
-			"// you can cleary hear the held low frequency t*32 here" CR
-			"// whereas the left side of the OR is sweeping wildly" CR
-			"[*] = (t*64 + a*s) | t*32",
-			// watches
-			"s", "a", "", "", "", "", "", "", "", "",
-		},
-
-		// this maybe takes too long to get interesting and is just unpleasant noise mostly, might want to ditch it.
-		{
-			"blurp",
-			50, 15, TTAlways,
-			0, 0, 0, 0, 0, 0, 0, 0,
-			"// if you can stand the noise, let this one run for a long time." CR
-			"// << shifts the bits of the left hand side" CR
-			"// by the number on the right hand side modulo 64" CR
-			"a = t<<t/(1024*8);" CR
-			"b = t>>t/16;" CR
-			"c = t>>t/32;" CR
-			"// & is bitwise AND" CR
-			"e = (a | b & c);" CR
-			"// we add 1 in two places here to prevent divide-by-zero errors" CR
-			"d = t%(t/512+1) + 1;" CR
-			"// remember that a is divided by d and then multiplied by 32" CR
-			"[*] = e/d * 32",
-			// watches
-			"a", "b", "c", "d", "e", "", "", "", "", "",
-		},
-
-		{
-			"garbage trash",
-			50, 15, TTAlways,
-			16, 0, 0, 0, 0, 0, 0, 0,
-			"// let this one run for a while, it subtly changes over time." CR
-			"// r will increase by one every V0+1 milliseconds." CR
-			"// we add one to V0 to prevent divide-by-zero." CR
-			"r = m/(V0+1);" CR
-			"// s will cycle through [0,15] at the same rate." CR
-			"s = r%16;" CR
-			"// note here that due to operator precedence," CR
-			"// t is multiplied by s and then the modulo is applied" CR
-			"[*] = (256*s + t*s%(512*s+1)) * r",
-			// watches
-			"r", "s", "", "", "", "", "", "", "", "",
-		},
-
-		{
-			"nonsense can",
-			50, 15, TTAlways,
-			0, 0, 0, 0, 0, 0, 0, 0,
-			"// this demonstrates using the previous value of the program" CR
-			"// in the calculation for output value" CR
-			"a = 1 + $(m)%32;" CR
-			"b = t*128 & t*64 & t*32;" CR
-			"c = (p/16) << p%4;" CR
-			"d = $(p/128) >> p%4;" CR
-			"p = a ^ b | c | d;" CR
-			"[*] = p;",
-			// watches
-			"a", "b", "c", "d", "p", "", "", "", "", "",
-		},
-
-		{
-			"moving average",
-			50, 17, TTAlways,
-			0, 0, 0, 0, 0, 0, 0, 0,
-			"// this sonifies the cumulative moving average for the function" CR
-			"// f(x) = x*256 ^ x*64 & x*32" CR
-			"// see https://en.wikipedia.org/wiki/Moving_average" CR
-			"x = t+1;" CR
-			"f = x*256 ^ x*64 & x*32;" CR
-			"p = p + (f - p)/x;" CR
-			"[*] = p;",
-			// watches
-			"x", "f", "p", "", "", "", "", "", "", "",
-		},
-
-		{
-			"stereo ellipse",
-			50, 22, TTAlways,
-			20, 100, 0, 0, 0, 0, 0, 0,
-			"// this sonifies the parametric equations for an ellipse." CR
-			"// the x-coordinate is sent to the left output channel." CR
-			"// the y-coordinate is sent to the right output channel." CR
-			"// V0 and V1 can be used to control the radii of the ellipse" CR
-			"a = V0+1;" CR
-			"b = V1+1;" CR
-			"// the cosine of t*128" CR
-			"c = $((t+w/2)*128);" CR
-			"// the sine of t*128;" CR
-			"s = $(t*128);" CR
-			"// assigning to [0] sets only the first output channel (left)" CR
-			"[0] = a*c;" CR
-			"// assigning to [1] sets only the second output channel (right)" CR
-			"[1] = b*s;",
-			// watches
-			"a", "b", "c", "s", "", "", "", "", "", "",
 		},
 
 		{
@@ -347,22 +406,6 @@ namespace Presets
 			"[0] = a; [1] = b;",
 			// watches
 			"V0", "s", "r", "a", "b", "", "", "", "", "",
-		},
-
-		{
-			"oink oink ribbit",
-			30, 18, TTProjectTime,
-			17, 2, 45, 5, 3, 0, 0, 0,
-			"// another example of using the previous value as part of the program," CR
-			"// with several V knobs incorporated" CR
-			"o = t*128;" CR
-			"a = t*V0 >> V1;" CR
-			"b = t - V2*100;" CR
-			"c = b*64 | b*V3>>V4;" CR
-			"p = (o | a) | c | p<<12;" CR
-			"[*] = p;",
-			// watches
-			"a", "b", "c", "p", "V0", "V1", "V2", "V3", "V4", "",
 		},
 
 		{
